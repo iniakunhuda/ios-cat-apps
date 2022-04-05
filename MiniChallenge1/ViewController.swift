68f105aaa74b0bed68b1170ad5480379b2276036
iniakunhuda <iniakunhuda@gmail.com>
2022-04-05 09:57:45 +0700

    Initial Commit

---
+++ MiniChallenge1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniChallenge1
+//
+//  Created by Miftahul Huda on 05/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
